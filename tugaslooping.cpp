f81dfdd85ae4c0ad76ef4a2abe328cb0e5cd4cdb
abiiiaja <kalindraaraya@gmail.com>
2026-04-01 10:48:31 +0700

    Pengecekan angka fibonacci dengan looping

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -18,5 +18,14 @@ bool BilPrim(int n) {
 
 bool BilFibo(int n) {
     int a = 0, b = 1, c = 0;
-
+    
+    while (c <= n) {
+        if (c == n) {
+            return true;
+        }
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return false;
 }
