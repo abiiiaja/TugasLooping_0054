24e477188b6b908f27b39d8f42b37a4b425a4987
abiiiaja <kalindraaraya@gmail.com>
2026-04-01 10:40:20 +0700

    Membuat fungsi menentukan bilangan prima (2): angka yang dimasukkan di tes apakah ada angka tersebut habis dibagi suatu bilangan dengan menggunan loop

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -7,4 +7,11 @@ bool BilPrim(int n) {
     if (n <= 1) return false;
 
     int i = 2;
+    while (i <= n / 2) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
 }
